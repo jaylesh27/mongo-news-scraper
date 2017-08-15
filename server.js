8890615813609c540600c504bdd00f0d0cf1e161
unknown <jaylesh27@gmail.com>
2017-08-15 15:23:32 -0400

    added scraping functionality

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// require npm package
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var request = require("request");
 var cheerio = require("cheerio");
+
+// require MongoDB models
 var Article = require('./models/article.js');
 var Comment = require('./models/comment.js');
 
@@ -45,15 +48,40 @@ app.get("/", function(req, res){
 
 app.get("/scrape", function(req, res){
 	request("https://arstechnica.com/", function(error, response, html) {
+		var result = {};
 		var $ = cheerio.load(html);
-		//console.log(html);
-		$("listing.listing-latest").each(function(i, element){
-			var result = {};
-			result.title = $(this).
+		$(".type-report.tease.article.tease-row").each(function(i, element){
+			//save the titles and linkes from the scraped website to the results object
+			result.title = $(this).children("header").find("a").text();
+			result.link = $(this).children("header").find("a").attr("href");
 			console.log(result);
+
+			var entry = new Article(result);
+
+			entry.save(function(err, doc){
+				if (err) {
+					console.log(err);
+				}else {
+					console.log(doc);
+				}
+			});
 		});
 	});
-	// console.log("route works");
+	res.send("Scrape Complete");
+});
+
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
 });
 
 
